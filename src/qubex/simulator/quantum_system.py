fix: refactor coupling label handling and improve effective frequency calculation in QuantumSystem

src/qubex/simulator/quantum_system.py
@@ -229,7 +229,7 @@ class QuantumSystem:
 
     @cache
     def get_coupling(self, label: str | tuple[str, str]) -> Coupling:
-        pair = label if isinstance(label, tuple) else tuple(label.split("-"))
+        pair = self.to_tuple_pair(label)
         if pair not in self.edge_set:
             if (pair[1], pair[0]) in self.edge_set:
                 pair = (pair[1], pair[0])
@@ -294,7 +294,7 @@ class QuantumSystem:
 
     @cache
     def get_coupling_detuning(self, label: str | tuple[str, str]) -> float:
-        pair = label if isinstance(label, tuple) else tuple(label.split("-"))
+        pair = self.to_tuple_pair(label)
         omega_0 = 2 * np.pi * self.get_object(pair[0]).frequency
         omega_1 = 2 * np.pi * self.get_object(pair[1]).frequency
         return omega_1 - omega_0
@@ -396,24 +396,36 @@ class QuantumSystem:
             raise ValueError(f"Invalid state alias: {alias}")
         return state
 
-    @cache
+    @staticmethod
+    def to_tuple_pair(label: str | tuple[str, str]) -> tuple[str, str]:
+        if isinstance(label, tuple):
+            return label
+        else:
+            pair = tuple(label.split("-"))
+            if len(pair) != 2:
+                raise ValueError(f"Invalid coupling label: {label}")
+            return pair
+
     def get_coupled_objects(self, label: str) -> list[Object]:
         if label not in self.node_set:
             raise ValueError(f"Object {label} does not exist.")
         neighbors = list(self.graph.neighbors(label))
         return [self.get_object(neighbor) for neighbor in neighbors]
 
-    @cache
+    def get_effective_frequency(self, label: str) -> float:
+        obj = self.get_object(label)
+        shift = self.get_frequency_shift(label)
+        return obj.frequency + shift
+
     def get_frequency_shift(self, label: str) -> float:
         shift = 0.0
         for neighbor in self.graph.neighbors(label):
             shift += self.get_lamb_shift((label, neighbor))
-            shift += self.get_residual_zz((label, neighbor))
+            shift += self.get_static_zz((label, neighbor))
         return shift
 
-    @cache
     def get_lamb_shift(self, label: str | tuple[str, str]) -> float:
-        pair = label if isinstance(label, tuple) else tuple(label.split("-"))
+        pair = self.to_tuple_pair(label)
         coupling = self.get_coupling(pair)
         obj_0 = self.get_object(pair[0])
         obj_1 = self.get_object(pair[1])
@@ -422,19 +434,17 @@ class QuantumSystem:
         delta = obj_0.frequency - obj_1.frequency
         return (g**2) / delta
 
-    @cache
-    def get_residual_zz(self, label: str | tuple[str, str]) -> float:
-        pair = label if isinstance(label, tuple) else tuple(label.split("-"))
-        coupling = self.get_coupling(pair)
+    def get_static_zz(self, label: str | tuple[str, str]) -> float:
+        pair = self.to_tuple_pair(label)
         obj_0 = self.get_object(pair[0])
         obj_1 = self.get_object(pair[1])
 
         if obj_0.dimension < 3 or obj_1.dimension < 3:
             return 0.0
 
-        g = coupling.strength
+        g = self.get_coupling(pair).strength
         delta = obj_0.frequency - obj_1.frequency
         alpha_0 = obj_0.anharmonicity
         alpha_1 = obj_1.anharmonicity
-        xi = g**2 * (alpha_0 + alpha_1) / ((delta + alpha_1) * (delta - alpha_0))
+        xi = g**2 * (alpha_0 + alpha_1) / ((delta + alpha_0) * (delta - alpha_1))
         return xi