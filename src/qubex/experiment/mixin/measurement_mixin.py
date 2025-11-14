aee915f55e9a0dce2000966be688a8a478fa5c65
Kengo Takemura <1225548@ed.tus.ac.jp>
2025-11-14 17:44:57 +0900

    Apply suggestions from code review

--- src/qubex/experiment/mixin/measurement_mixin.py
+++ src/qubex/experiment/mixin/measurement_mixin.py
@@ -4922,73 +4922,86 @@ class MeasurementMixin(
         )
 
     def rzx_gate_property(
-        self,
-        control_qubit: str,
-        target_qubit: str,
-        *,
-        angle_arr:np.ndarray = np.linspace(np.pi/18,np.pi/2,9),
-        measurement_times:int = 10,
-    ):
+            self,
+            control_qubit: str,
+            target_qubit: str,
+            *,
+            angle_arr: np.ndarray = np.linspace(np.pi/18,4*np.pi/9,8),
+            measurement_times: int = 10,
+    ) -> Result:
+        
+        RAD_TO_DEG = 180 / np.pi
+
         def cartesian_to_spherical(x, y, z):
             r = np.sqrt(x**2 + y**2 + z**2)
-            theta = np.arctan2(y, x)/np.pi*180
-            phi = np.arccos(z / r)/np.pi*180 if r != 0 else 0
+            theta = np.arctan2(y, x)*RAD_TO_DEG
+            phi = np.arccos(z / r)*RAD_TO_DEG if r != 0 else 0
             return r, theta, phi
         
         result_rzx_angle=[]
-        for angle in tqdm(angle_arr,leave=False):
-            result_mea=[]
-            for i in tqdm(range(measurement_times),leave=False):
+        for angle in tqdm(angle_arr, leave=False):
+            results=[]
+            for _ in tqdm(range(measurement_times), leave=False):
                 result = self.state_tomography(self.rzx(
                     control_qubit=control_qubit,
                     target_qubit=target_qubit,
                     angle=angle))
-                r,theta,phi = cartesian_to_spherical(result[target_qubit][0],result[target_qubit][1],result[target_qubit][2])
-                result_mea.append([r,theta,phi])
-            mean = np.mean(np.array(result_mea).T[2])
-            std = np.std(np.array(result_mea).T[2])
+                x, y, z = result[target_qubit]
+                r,theta,phi = cartesian_to_spherical(x, y, z)
+                results.append([r,theta,phi])
+            result_array = np.array(results)
+            mean = np.mean(result_array[:, 2])
+            std = np.std(result_array[:, 2])
             result_rzx_angle.append([angle,mean,std])
         
         fig = go.Figure()
         fig.add_trace(
             go.Scatter(
-                x = np.array(result_rzx_angle).T[0]*180/np.pi,
+                x = np.array(result_rzx_angle).T[0]*RAD_TO_DEG,
                 y = np.array(result_rzx_angle).T[1],
+                marker=dict(color=COLORS[1]),
                 error_y=dict(
                     type='data',
-                    array=np.array(result_rzx_angle).T[2]
+                    array=np.array(result_rzx_angle).T[2],
+                    color = COLORS[1]
                 ),
                 name='Measured'
             ),
         )
         fig.add_trace(
             go.Scatter(
-                x = [0,95],
-                y = [0,95],
+                x = [0,90],
+                y = [0,90],
                 mode='lines',
-                line=dict(color='red', dash='dash'),
+                line=dict(color=COLORS[0], dash='dash'),
                 name='Ideal'
             )
         )
         fig.update_layout(
-            title=f'Sweep result : {target_qubit}',
+            title=f'Sweep result : {control_qubit}-{target_qubit}',
             xaxis=dict(
                 title = "Angle (deg)",
-                range =(0,95),
-                tickvals=angle_arr*180/np.pi,
+                range =(0,90),
+                tickvals=angle_arr*RAD_TO_DEG,
                 dtick=5,
                 gridcolor='gray',
-                gridwidth=1,
+                gridwidth=3,
                 griddash='dot'
             ),
             yaxis=dict(
                 title="Z Angle (deg)" ,
-                range=(0,95),
-                tickvals=angle_arr*180/np.pi,
+                range=(0,90),
+                tickvals=angle_arr*RAD_TO_DEG,
                 dtick=5,
                 gridcolor='gray',
-                gridwidth=1,
+                gridwidth=3,
                 griddash='dot'
             )
         )
         fig.show()
+        return Result(
+            data={
+                "data": results,
+                "figure": fig,
+            }
+        )
