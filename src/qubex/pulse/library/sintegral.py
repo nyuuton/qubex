fix: correct scaling in Sintegral class and adjust value handling for odd/even cases

src/qubex/pulse/library/sintegral.py
@@ -93,7 +93,9 @@ class Sintegral(Pulse):
             n=power,
         )
         Omega -= sin_pow_integral(0, n=power)
-        scale = amplitude / sin_pow_integral(np.pi, n=power)
+        scale = amplitude / (
+            sin_pow_integral(np.pi, n=power) - sin_pow_integral(0, n=power)
+        )
         Omega *= scale
         if beta is None:
             values = Omega
@@ -112,7 +114,7 @@ class Sintegral(Pulse):
             np.where(
                 (t <= duration // 2),
                 values,
-                values if is_odd else 2 - values,
+                values if is_odd else 2 * amplitude - values,
             ),
             0,
         ).astype(np.complex128)